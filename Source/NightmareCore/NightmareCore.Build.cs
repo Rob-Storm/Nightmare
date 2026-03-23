b9520068528e1ff667dccff7c6d8f190a64833f2
The1Wolfcast <the1wolfcast@gmail.com>
2026-03-23 12:39:49 -0500

    trying to fix packaging issues

--- Source/NightmareCore/NightmareCore.Build.cs
+++ Source/NightmareCore/NightmareCore.Build.cs
@@ -7,7 +7,8 @@ public class NightmareCore : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 		bEnforceIWYU = true;	
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "UMG",
+		"OnlineSubsystem", "OnlineSubsystemUtils", "OnlineSubsystemSteam", "OnlineSubsystemNull", "Voice" });
 		PrivateDependencyModuleNames.AddRange(new string[] { });
 	}
 }
