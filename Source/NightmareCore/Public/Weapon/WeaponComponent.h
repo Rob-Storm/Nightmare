85f3bd5177f6c893b6c9bf5140e6aeb3fcc84e2d
The1Wolfcast <the1wolfcast@gmail.com>
2026-03-26 23:58:23 -0500

    starting work on weapons

---
+++ Source/NightmareCore/Public/Weapon/WeaponComponent.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+
+#include "WeaponComponent.generated.h"
+
+UCLASS()
+class UWeaponComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	
+};
